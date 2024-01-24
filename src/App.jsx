4869bdb88ee6ef87fe0c610a9fd15e8065c119e3
Ameer jafar <ameerjafar123@gmail.com>
2024-01-24 21:05:59 +0530

    adding several code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { ParagraphGenerator } from './Component/ParagraphGenerator'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <ParagraphGenerator></ParagraphGenerator>
-    </>
+    <div>
+      <ParagraphGenerator/>
+    </div>
   )
 }
 
--- src/Component/ParagraphGenerator.jsx
+++ src/Component/ParagraphGenerator.jsx
@@ -1,10 +1,34 @@
-export function ParagraphGenerator({setWords}) {
-    return (
-        <div>
-            <h3>para generator</h3>
-            <input type = "text" placeholder="Enter your paragraph size"></input>
-            <button>Generator</button>
-            <p>{setWords}</p>
-        </div>
-    )
-}
+import React, { useState } from 'react';
+
+export const ParagraphGenerator = () => {
+  const [fileContent, setFileContent] = useState('');
+  const fileName = 'paragraph.txt';
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(fileName, {
+        headers: {
+        'Content-Type': 'text/plain'
+        }
+      })
+      if(response.ok) {
+        const content = await response.text();
+        setFileContent(content);
+        console.log(content);
+        console.log("the file is fetch properly");
+      }else {
+        console.log("The file is not fetch properly");
+      }
+    }
+    fetchData();
+  }, [fileContent]);
+  return (
+    <div>
+      <h2>Para Generator</h2>
+      <input type = "text" placeholder='Enter size of your paragraph'></input>
+      <button>Generate</button>
+      <p>{fileContent}</p>
+    </div>
+  )
+};
+
+
